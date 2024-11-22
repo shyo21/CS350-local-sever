31d0878e88e2e0763dbdaf6e55aa0ab80742f9d0
shyo21 <shyo21@kaist.ac.kr>
2024-11-23 02:03:37 +0900

    alembic modified

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os
+import sys
+
+# 프로젝트 루트 디렉토리를 파이썬 경로에 추가
+sys.path.append(os.path.join(sys.path[0], 'api'))
+
+# SQLAlchemy Base 메타데이터 가져오기
+from api.models import Base  # Base를 import
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,12 +27,15 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+# 환경 변수 로드 (필요 시)
+from dotenv import load_dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 
 def run_migrations_offline() -> None:
